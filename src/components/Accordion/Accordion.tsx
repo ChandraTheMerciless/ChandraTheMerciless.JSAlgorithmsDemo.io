493ce1191af24fd65ab6dd7fcb8e2c82be7a875a
chandrathemerciless <chandra.asar@gmail.com>
2024-10-01 17:28:04 -0400

    Fixed max height on open accordion to be dynamically calculated

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
 import classes from 'components/Accordion/Accordion.module.css';
 import { ReactComponent as Chevron } from 'assets/8665193_chevron_down_icon.svg';
 
@@ -9,10 +9,18 @@ interface AccordionProps {
 
 export default function Accordion({ title, children }: AccordionProps) {
   const [isOpen, setIsOpen] = useState(false);
+  const [maxHeight, setMaxHeight] = useState('0px');
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const onOpenClick = () => {
     setIsOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setMaxHeight(isOpen ? `${contentRef.current.scrollHeight}px` : '0px');
+    }
+  });
 
   return (
     <div className={classes.accordion}>
@@ -27,8 +35,11 @@ export default function Accordion({ title, children }: AccordionProps) {
             ? `${classes.expandable} ${classes.active}`
             : classes.expandable
         }
+        style={{ maxHeight }}
       >
-        <div className={classes.content}>{children}</div>
+        <div className={classes.content} ref={contentRef}>
+          {children}
+        </div>
       </div>
     </div>
   );
--- src/components/Algorithms/AlgorithmGroup.test.tsx
+++ src/components/Algorithms/AlgorithmGroup.test.tsx
@@ -3,10 +3,20 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import AlgorithmGroup from './AlgorithmGroup';
 
-test('correctly renders loose comparison for truthy', async () => {
-  render(<AlgorithmGroup algorithm="42 == '42'" />);
-  const buttonElement = screen.getByTestId('calculateButton');
-  await userEvent.click(buttonElement);
-  const answer = screen.getByText(/true/i);
-  expect(answer).toBeInTheDocument();
+describe('AlgorithmGroup', () => {
+  test('correctly renders loose comparison for truthy', async () => {
+    render(<AlgorithmGroup algorithm="42 == '42'" />);
+    const buttonElement = screen.getByTestId('calculateButton');
+    await userEvent.click(buttonElement);
+    const answer = screen.getByText(/true/i);
+    expect(answer).toBeInTheDocument();
+  });
+
+  test('correctly renders strict comparison for falsy', async () => {
+    render(<AlgorithmGroup algorithm="42 === '42'" />);
+    const buttonElement = screen.getByTestId('calculateButton');
+    await userEvent.click(buttonElement);
+    const answer = screen.getByText(/false/i);
+    expect(answer).toBeInTheDocument();
+  });
 });
