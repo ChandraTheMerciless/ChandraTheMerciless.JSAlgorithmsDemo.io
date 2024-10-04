93b2a27847f1ae8aba3ffa24f8f503d203bcd8f6
chandrathemerciless <chandra.asar@gmail.com>
2024-10-04 15:28:23 -0400

    Fixed some styling for the algorithm group

--- src/components/Algorithms/AlgorithmGroup.tsx
+++ src/components/Algorithms/AlgorithmGroup.tsx
@@ -8,21 +8,32 @@ interface AlgorithmGroupProps {
 
 export default function AlgorithmGroup({ algorithm }: AlgorithmGroupProps) {
   const [answer, setAnswer] = useState();
+  const [isAnswered, setIsAnswered] = useState(false);
 
   const getAnswer = () => {
     // eslint-disable-next-line no-eval
     const parsed = eval(algorithm);
     setAnswer(typeof parsed === 'boolean' ? parsed.toString() : parsed);
+    setIsAnswered(true);
   };
 
   return (
     <div className={classes['algorithm-group']}>
       <div className={classes.content}>
         <p>{algorithm}</p>
-        <button data-testid="calculateButton" onClick={getAnswer}>
-          =
-        </button>
-        <p>{answer}</p>
+        {isAnswered ? (
+          <p className={classes.answer}>{`// ${answer}`}</p>
+        ) : (
+          <div>
+            <button
+              aria-label={`${algorithm} equals`}
+              data-testid="calculateButton"
+              onClick={getAnswer}
+            >
+              =
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
